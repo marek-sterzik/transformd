9d68b14b61d85a4f98bb505de2fb615c48033185
Marek Sterzik <marek.sterzik@gmail.com>
2024-04-19 22:13:16 +0200

    added "Powered by TransforMD" link to the footer

--- source/templates/foot.php
+++ source/templates/foot.php
@@ -1,4 +1,8 @@
-<?php if ($homeUrl !== null): ?>
     <hr />
-    <p><a href="<?php echo htmlspecialchars($homeUrl); ?>"><?php echo htmlspecialchars($homeCaption);?></a></p>
-<?php endif ?>
+    <div class="foot">
+    <?php if ($homeUrl !== null): ?>
+        <div class="home"><a href="<?php echo htmlspecialchars($homeUrl); ?>"><?php echo htmlspecialchars($homeCaption);?></a></div>
+    <?php endif ?>
+        <div class="powered">Powered by <a href="https://github.com/marek-sterzik/transformd" target="_blank">TransforMD</a></div>
+        <div class="clear"></div>
+    </div>
